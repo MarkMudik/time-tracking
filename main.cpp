e5f6da5533adec7d8c5ffe0ab73036f1eedec2d8
MarkieAurelius <markieaurelius@gmail.com>
2025-05-24 21:59:40 -0700

    Start of project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
+#include "time.h"
+#include <iostream>
+using namespace std;
+void menu();
 
 int main() {
+
+    menu();
+
     return 0;
+}
+
+void menu() {
+    cout << "(1) Enter time" << std::endl;
+    cout << "(2) Calendar" << std::endl;
 }
---
+++ output.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+void startMenu();
+void inputDateAndTimeMenu();
+void outputCalendar();
+
+#endif
